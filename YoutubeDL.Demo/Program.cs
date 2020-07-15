3764968f785d3e1b7794e40d77bdab1b7728e723
SnGmng <38666407+SnGmng@users.noreply.github.com>
2020-07-15 15:59:12 +0200

    edit demo

--- YoutubeDL.Demo/Program.cs
+++ YoutubeDL.Demo/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using YoutubeDL;
 using YoutubeDL.Models;
+using YoutubeDL.Python;
 
 namespace youtube_dl_net_demo
 {
@@ -26,10 +27,16 @@ namespace youtube_dl_net_demo
         public static async Task Test()
         {
             YouTubeDL ytdl = new YouTubeDL();
+            await ytdl.CheckDownloadYTDLPython(false);
+            ytdl.AddPythonExtractors();
+            ytdl.Options.Format = "bestvideo+m4a/best";
+            //ytdl.Options.ExtractFlat = "in_playlist";
+            //ytdl.Options.MergeOutputFormat = "mp4";
             //ytdl.Options.Format = "bestaudio";
             //ytdl.Options.Verbose = true;
-            //InfoDict dict = ytdl.ExtractInfo("https://www.youtube.com/playlist?list=PL8SwD_foum9yWCuNn1IyqkZI7EMmnzxcr", download: false);
-            InfoDict dict = await ytdl.ExtractInfoAsync("https://www.youtube.com/watch?v=X1jMMFOqxEw");
+            //InfoDict dict = await ytdl.GetSearchResults("far out - overdrive", 10);
+            //InfoDict dict = await ytdl.ExtractInfoAsync("https://www.youtube.com/playlist?list=PL8SwD_foum9yWCuNn1IyqkZI7EMmnzxcr", download: false);
+            InfoDict dict = await ytdl.ExtractInfoAsync("https://www.youtube.com/watch?v=X1jMMFOqxEw"); //https://www.youtube.com/watch?v=oP8TAcUc17w");
 
             if (dict is Video video)
             {
@@ -38,6 +45,16 @@ namespace youtube_dl_net_demo
             if (dict is Playlist playlist)
             {
                 Console.WriteLine("YoutubeDL for .NET Extracted Playlist " + playlist.Id + ": " + playlist.Title);
+                foreach (ContentUrl d in playlist.Entries)
+                {
+                    //Console.WriteLine(d.GetType().Name + ":");
+                    foreach (var prop in d.AdditionalProperties)
+                    {
+                        //Console.WriteLine(prop.Key + " = " + prop.Value);
+                    }
+                }
+
+                //await ytdl.ProcessIEResult(playlist.Entries[0], true);
             }
 
             //InfoDict dict2 = ytdl.ExtractInfo("https://www.youtube.com/watch?v=X1jMMFOqxEw");
