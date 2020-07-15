02dd2b3314b7acbd3228637df514f15488076a68
SnGmng <38666407+SnGmng@users.noreply.github.com>
2020-07-15 15:48:00 +0200

    Add summaries to YTDLMetaAttribute

--- YoutubeDL/Attributes.cs
+++ YoutubeDL/Attributes.cs
@@ -1,19 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace YoutubeDL
 {
     /// <summary>
-    /// This Attribute indicates what the equivalent name in the youtube-dl python version is.
+    /// This Attribute stores metadata of properties for youtube-dl
     /// </summary>
     public sealed class YTDLMetaAttribute : Attribute
     {
+        /// <summary>
+        /// The equivalent name in the youtube-dl python version (for auto-fill)
+        /// </summary>
         public string PythonName { get; set; }
+
+        /// <summary>
+        /// Command line argument name (ex: --download)
+        /// </summary>
         public string ArgName { get; set; }
+
+        /// <summary>
+        /// Short command line argument name (ex: -d)
+        /// </summary>
         public string ShortArgName { get; set; }
+
+        /// <summary>
+        /// A description that is shown by the "help" command/function
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Controls if the value is assicgned automatically if the names match or manually by corresponding model constructor. 
+        /// </summary>
         public bool AutoFill { get; set; } = true;
+
         public YTDLMetaAttribute(string pythonName, string shortArgName = null)
         {
             this.PythonName = pythonName;
